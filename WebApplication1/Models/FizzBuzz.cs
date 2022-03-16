b65f75109c43dcfcae97dd3d99fc738652d3a320
slik margor <slikmargor@gmail.com>
2022-03-16 14:47:59 +0100

    <Adding ModeState Validation>

---
+++ WebApplication1/Models/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class FizzBuzz
+    {
+        [Required, Range(1, 100, ErrorMessage ="Podaj dobry zakres")]
+        [Display(Name = "Podaj mi swój numerek")]
+        public int? Number { get; set; }
+
+        public string CheckRange(int? number)
+        {
+            if (number is null)
+                return "Liczba nie spełnia parametru";
+            else
+            {
+                if (number % 15 == 0)
+                    return "FizzBuzz";
+                else if (number % 5 == 0)
+                    return "Buzz";
+                else if (number % 3 == 0)
+                    return "Fizz";
+                else
+                    return "Zła liczba";
+            }
+        }
+    }
+}
--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,20 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public string Name { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private IndexModel()
+        {
+
+        }
+        public IndexModel(ILogger<IndexModel> logger):this()
         {
             _logger = logger;
+            FizzBuzz = new FizzBuzz();
         }
 
         public void OnGet()
         {
-
+            /*if(Name is null)
+            {
+                Name = "Byku";
+            }*/
         }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
     }
 }
